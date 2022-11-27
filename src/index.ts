a317566f4a3282bab3f6f8214cf1a46356315720
Stijn Mertens <stijn.mertens@expertum.net>
2022-11-27 16:23:40 +0100

    feat: player id is now a parameter

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ const cocapi = require('./coc_api');
 const config = require('./config');
 
 
-async function get_player(): Promise <void> {
+async function get_player(id:string): Promise <void> {
     console.log("get_player()");
 
     try {
       const api = new cocapi(config.token);
-      const data = await api.get("/players/%23LOLYC9UYQ");
+      const data = await api.get("/players/" + id);
       console.log("Found player: " + data.name + "\n" +
                   "Clan: " + data.clan.name);
     } catch (error) {
@@ -17,5 +17,5 @@ async function get_player(): Promise <void> {
 }
 
 
-get_player();
+get_player("%23LOLYC9UYQ");
 
