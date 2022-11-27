8a576dae0c980c85ec98ad16af5e1c8f7e1c0f86
Stijn Mertens <stijn.mertens@expertum.net>
2022-11-27 16:05:39 +0100

    feat: API token now saved in config file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 //const axios = require('axios').default;
 const cocapi = require('./coc_api');
+const config = require('./config');
 
 
 async function get_player(): Promise <void> {
     console.log("get_player()");
 
     try {
-      const api = new cocapi("insert api token here");
+      const api = new cocapi(config.token);
       const data = await api.get("/players/%23LOLYC9UYQ");
       console.log("Found player: " + data.name + "\n" +
                   "Clan: " + data.clan.name);
