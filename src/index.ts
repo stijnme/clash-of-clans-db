35f18685a55891da05b8aa0bde846a3b85382746
Stijn Mertens <stijn.mertens@expertum.net>
2022-12-06 20:56:47 +0100

    feat: POC with API retrieval and write to SQLite DB

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,45 @@
-export { };
+import { normalize } from "path";
 
-//const axios = require('axios').default;
-const player = require('./player');
+export {};
 
-const player1 = new player();
-player1.get("%23LOLYC9UYQ");
-console.log("Found player1: " + player1.name);
+const Player = require('./player');
+//const PlayersDB = require('./model/players');
+const { Sequelize, DataTypes } = require('sequelize');
+
+async function main() {
+    const player1 = new Player();
+    await player1.get("%23LOLYC9UYQ");
+    console.log("Found player1: " + player1.name);
+
+    // try sequilize
+    if (player1 !== undefined) {
+        // define db
+        const sequelize = new Sequelize({
+            dialect: 'sqlite',
+            storage: './db/ClashOfClans.db'
+        });
+
+        // define Players model
+        const Players = sequelize.define('Players',
+            {
+                tag: { primaryKey:true, allowNull: false, type: Sequelize.STRING },
+                name: { type: Sequelize.STRING },
+            },
+            { timestamps: true }
+        );
+        console.log(Players === sequelize.models.Players);
+        await Players.sync(); // create table in DB if needed
+
+        // create new player object
+        const newPlayer = {
+            tag: player1.tag,
+            name: player1.name
+        };
+        
+        // insert/update in db
+        Players.upsert(newPlayer);
+    }
+}
 
 
-//const player2 = new player("%23QPYG9GCJL");
+main();
