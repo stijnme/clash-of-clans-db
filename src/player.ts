547dd62757f313e25628dd0fb0fd7ab2b54f238c
Stijn Mertens <stijn.mertens@expertum.net>
2022-12-28 22:12:47 +0100

    update: player class

--- src/player.ts
+++ src/player.ts
@@ -1,11 +1,47 @@
-import { CocAPI } from './coc_api';
+import { CocAPI } from './api/coc_api';
 import Config from './config';
 
 export class Player {
     public tag: string | undefined;
     public name: string | undefined;
 
-    async get(tag:string): Promise <void> {
+    constructor(tag:string){
+        this.tag = tag;
+    }
+
+    async get(): Promise <void> {
+        console.log("get_player()");
+
+        try {
+            const api = new CocAPI(Config.token);
+            // TODO: create interface/type for the player data
+            const data: any = await api.get("/players/" + this.tag);
+
+            console.log("Found player: " + data.name + "\n" +
+                        "Clan: " + data.clan.name);
+            this.name = data.name;
+            this.tag = data.tag;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+
+/* import { CocAPI } from './coc_api';
+import Config from './config';
+import { Sequelize, DataTypes, Model } from 'sequelize';
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db/ClashOfClans.db'
+});
+
+class Player extends Model {
+    declare tag: string | undefined;
+    declare name: string | undefined;
+
+    async request(tag:string): Promise <void> {
         console.log("get_player()");
     
         try {
@@ -22,3 +58,16 @@ export class Player {
         }
     }
 }
+
+Player.init(
+    {
+        tag: { primaryKey:true, allowNull: false, type: DataTypes.STRING },
+        name: { type: DataTypes.STRING },
+    },
+    //{ timestamps: true }
+    {
+        sequelize: sequelize
+    }
+);
+
+export default Player; */
