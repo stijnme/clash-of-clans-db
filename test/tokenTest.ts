eba0b7e7d6378f9d9d0788cd2f8baf074c1108d2
Stijn Mertens <stijn.mertens@expertum.net>
2023-02-20 15:32:26 +0100

    test: back to uppercase...

--- test/tokenTest.ts
+++ test/tokenTest.ts
@@ -2,7 +2,7 @@ const assert = require("chai").assert;
 
 describe("Verify token retrieval from environment variables", function () {
   it("Check if token differs from undefined", async function () {
-    const token = process.env.token;
+    const token = process.env.TOKEN;
     assert.exists(token, "TOKEN is not undefined");
   });
 });
